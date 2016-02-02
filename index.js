b26289f5a08e3aa87796fba86ef7ef62d878e305
Justin Fargione <justin@lexicalbits.com>
2016-02-02 12:15:35 -0500

    Applied the output fix to the correct function, removed old dbox code

--- index.js
+++ index.js
@@ -17,32 +17,6 @@ module.exports = {
                 return this.fail(error);
             }
             self.complete(_.map(reply, function(meta) {
-                return {
-                    source: 'dropbox'
-                    , size: meta.bytes
-                    , path: meta.path
-                    , id: null
-                    , created: null
-                    , modified: meta.modified
-                };
-            }));
-
-        });
-    },
-    dboxRun: function(step, dexter) {
-        var path = step.input('path').first()
-            , token = dexter.provider('dropbox').credentials('access_token')
-            , client = require('dbox').app({}).client(token)
-            , self = this
-        ;
-        client.search(path, '.jpg', {
-            file_limit: 1000
-        }, function(status, reply) {
-            if(status !== 200) {
-                console.error(reply);
-                return self.fail('Photo search failed with status ' + status);
-            }
-            self.complete(_.map(reply, function(meta) {
                 return { photo: {
                     source: 'dropbox'
                     , size: meta.bytes
