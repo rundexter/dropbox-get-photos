9557642ed042e3bc43749b5818bbfde1a6bc64a1
Justin Fargione <justin@lexicalbits.com>
2016-02-03 12:06:13 -0500

    Nodeified, added png and gif support

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash')
     , dropbox = require('dropbox')
+    , q = require('q')
 ;
 module.exports = {
     run: function(step, dexter) {
@@ -8,25 +9,33 @@ module.exports = {
             , client = new dropbox.Client({
                 token: token
             })
+            , search = q.nbind(client.search, client)
             , self = this
         ;
-        client.search(path, '.jpg', {
-            file_limit: 1000
-        }, function(error, reply) {
-            if(error) {
-                return this.fail(error);
-            }
-            self.complete(_.map(reply, function(meta) {
-                return { photo: {
-                    source: 'dropbox'
-                    , size: meta.bytes
-                    , path: meta.path
-                    , id: null
-                    , created: null
-                    , modified: meta.modified
-                }};
-            }));
-
-        });
+        q.all([
+            search(path, '.jpg')
+            , search(path, '.png')
+            , search(path, '.gif')
+        ])
+            .then(function(results) {
+                all = _.union.apply(null, results);
+                self.complete(
+                    _.map(
+                        all
+                        , function(meta) {
+                            return { photo: {
+                                source: 'dropbox'
+                                , size: meta.bytes
+                                , path: meta.path
+                                , id: null
+                                , created: null
+                                , modified: meta.modified
+                            }};
+                        }
+                    )
+                );
+            })
+            .fail(self.fail)
+        ;
     }
 };
